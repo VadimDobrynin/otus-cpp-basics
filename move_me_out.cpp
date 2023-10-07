ba15fc9956eb3944816a295a8ff380967ebcc975
Vadim Dobrynin <vadim_dobrunin@mail.ru>
2023-10-07 22:45:06 +0300

    добавлен проект. Даже работаÑаетÐÐ:)

---
+++ move_me_out.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Hp on 07.10.2023.
+//
+#include "move_me_out.h"
+#include <iostream>
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ move_me_out.h
@@ -0,0 +1,10 @@
+//
+// Created by Hp on 07.10.2023.
+//
+
+#ifndef INC_01_HELLO_WORLD_MOVE_ME_OUT_H
+#define INC_01_HELLO_WORLD_MOVE_ME_OUT_H
+
+void todo_move_me_out();
+
+#endif //INC_01_HELLO_WORLD_MOVE_ME_OUT_H
